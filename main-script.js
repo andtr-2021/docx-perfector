39fd91cde6efe5faf24f14547bdbffd3cbf677f8
jaeheon <jjheon0614@gmail.com>
2024-01-07 16:00:05 +0700

    processing-dot

--- main-script.js
+++ main-script.js
@@ -20,3 +20,22 @@ logoutBtn.addEventListener('click', () => {
     window.location.href = 'landing-page.html';
 });
 
+
+document.querySelector('.submitButton').addEventListener('click', function() {
+    // Show the processing message
+    document.querySelector('.bot-response').style.display = 'flex';
+
+    var processingText = document.getElementById('processing-text');
+    var dotCount = 0;
+
+    // Function to update the text
+    function updateProcessingText() {
+        var dots = '.'.repeat(dotCount);
+        processingText.textContent = 'Processing the file' + dots;
+        dotCount = (dotCount % 3) + 1; // Reset the count after 5 dots
+    }
+
+    // Start the interval
+    var intervalId = setInterval(updateProcessingText, 1000);
+
+});
